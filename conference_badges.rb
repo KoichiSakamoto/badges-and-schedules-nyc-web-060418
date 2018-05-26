986b98fea5d0342823555d8a5c3350be76b9d528
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-26 15:46:31 -0400

    implemented methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  returnArray = array.collect {|name| "Hello, my name is #{name}."}
+  returnArray
+end
+
+def assign_rooms(array)
+  counter = 1
+  returnArray =[]
+  array.each do |name|
+    returnArray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter +=1
+  end
+  returnArray
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |element|
+    puts element
+  end
+  assign_rooms(array).each do |element|
+    puts element
+  end
+end
